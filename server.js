584038c373d20bebabc0bb39676a700747b20a40
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-10-21 20:09:45 -0400

    changes the health check ok

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK',
+        app: 'OKkk',
         mongo: mongoConnected
     };
     res.json(stat);
